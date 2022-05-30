e682211decea8f600a758e6a9ecdc594d2415b7f
mesutsemiz <semihturgutlu@gmail.com>
2022-05-30 11:37:16 +0300

    3 files modified(table wsa ceated)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import "./home.scss"
 import Widget from "../../components/widget/Widget"
 import Featured from "../../components/featured/Featured"
 import Chart from "../../components/chart/Chart"
+import Table from "../../components/table/Table"
 
 const Home=()=>{
   return(
@@ -24,6 +25,7 @@ const Home=()=>{
         </div>
         <div className="listContainer">
           <div className="listTitle">Latest Transactions</div>
+          <Table/>
         </div>
       </div>
     </div>
