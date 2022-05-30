36accd7131cc91eaa45e04847c9dbbf41d5ed42f
mesutsemiz <semihturgutlu@gmail.com>
2022-05-30 13:29:34 +0300

    datattable in proccess(part2)

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,47 +1,15 @@
 import React from "react";
 import "./datatable.scss"
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-
-const columns= [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+import { DataGrid} from '@mui/x-data-grid';
+import {userRows,userColumns} from "../../datatablesource"
 
 
 const Datatable=()=>{
   return(
     <div className="datatable">
        <DataGrid
-        rows={rows}
-        columns={columns}
+        rows={userRows}
+        columns={userColumns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
@@ -50,3 +18,5 @@ const Datatable=()=>{
     </div>
   )
 }
+
+export default Datatable
